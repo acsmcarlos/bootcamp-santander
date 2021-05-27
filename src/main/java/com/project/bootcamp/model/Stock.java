4d0facf17fd97a1e2476777a6237b03925fdb80b
acsmcarlos <acsmcarlos@gmail.com>
2021-05-27 17:46:22 -0300

    Projeto criado para o bootcamp santander dev week

---
+++ src/main/java/com/project/bootcamp/model/Stock.java
@@ -0,0 +1,64 @@
+package com.project.bootcamp.model;
+
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_stock")
+public class Stock {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	private String name;
+	private Double price;
+	private Double variation;
+	private LocalDate date;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public Double getVariation() {
+		return variation;
+	}
+
+	public void setVariation(Double variation) {
+		this.variation = variation;
+	}
+
+	public LocalDate getDate() {
+		return date;
+	}
+
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+
+}
---
+++ src/main/java/com/project/bootcamp/util/MessageUtils.java
@@ -0,0 +1,9 @@
+package com.project.bootcamp.util;
+
+public abstract class MessageUtils {
+	
+	public static final String STOCK_ALREADY_EXISTS = "O Strock já existe no banco de dados!";
+	
+	public static final String NO_RECORDS_FOUND = "O registro solicitado não existe na base de dados do sistema!";
+
+}
